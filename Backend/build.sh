24db8c144653ae464038e9ba22729774755c8018
Rfusar <rfusaro12@gmail.com>
2025-07-13 18:27:08 +0200

    update

--- Backend/build.sh
+++ Backend/build.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-sudo docker build --tag rfusaro12276/backend
+sudo docker build --tag rfusaro12276/backend .
--- Frontend/build.sh
+++ Frontend/build.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-sudo docker build --tag rfusaro12276/frontend
+sudo docker build --tag rfusaro12276/frontend .
+sudo docker push rfusaro12276/frontend
