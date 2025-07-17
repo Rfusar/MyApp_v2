97ef78a533fc121ffdbc3e006cfe01d9649131c7
Rfusar <rfusaro12@gmail.com>
2025-07-17 23:23:59 +0200

    update

--- Services/Documents/main.py
+++ Services/Documents/main.py
@@ -17,43 +17,39 @@ class Doc:
     def __init__(self):
         self.config = {
             "dpi": config["system"]["dpi"],
-            "MinConfidance": config["system"]["MinConfidance"]
+            "MinConfidance": config["system"]["MinConfidance"],
+            "onlyClassifications": config["system"]["onlyClassifications"],
+            "allText": config["system"]["allText"],
         }
         self.folders = config["folders"]
         
         for name, path in self.folders.items():
-            if name != "input":
-                os.makedirs(path, exist_ok=True)
-            else:
-                if not os.path.exists(path):
-                    print("Devi creare la cartella di input con i file PDF, nella stessa cartella dell'eseguibile")
-
-
+            os.makedirs(path, exist_ok=True)
+        if not os.path.exists(self.folders["input"]):
+            print("Devi creare la cartella di input con i file PDF o immagini.")
 
     def ocr(self, filepath: str):
         reader = easyocr.Reader(['it', 'en'])
         img_np = None
-    
+
         try:
             if filepath.lower().endswith(".pdf"):
                 pages = convert_from_path(filepath, dpi=self.config["dpi"])
                 img_np = np.array(pages[0])
             else:
-                img = Image.open(filepath)
-                img = img.convert("RGB")  # forza RGB se in scala di grigi o CMYK
+                img = Image.open(filepath).convert("RGB")
                 img_np = np.array(img)
         except Exception as e:
             self.Log(f"[ERROR] File non leggibile: {filepath} -> {e}")
             return []
-    
+        
         return reader.readtext(img_np)
-    
+
     def Log(self, content: str) -> None:
         filelog, timelog = dt.now().strftime("%Y_%m_%d___%H:%M:%S").split("___")
         log_path = os.path.join(self.folders["log"], f"{filelog}.txt")
-        with open(log_path, "a+") as f:
+        with open(log_path, "a+", encoding="utf-8") as f:
             f.write("[" + timelog + "] " + content + "\n")
-
 
 class Tari(Doc):
     def __init__(self):
@@ -71,28 +67,32 @@ class Tari(Doc):
                         break
         return result
 
-
 # Esecuzione principale
 MyDocument = Tari()
 
 for filepath in glob.glob(os.path.join(MyDocument.folders["input"], "*")):
     results = MyDocument.ocr(filepath)
     
+    output = []
     for bbox, text, conf in results:
+        if MyDocument.config["allText"] == True: MyDocument.Log(text)
+        
         if conf >= MyDocument.config["MinConfidance"]:
-            matches = MyDocument.classify_row(text)
-            log = (
-                "############ NEW ROW ###########\n"
-                f"File: {os.path.basename(filepath)}\n"
-                f"Confidence: {conf:.2f}\n"
-                f"Coordinate X: {bbox[0][0]}, Y: {bbox[0][1]}\n"
-                "Text: ````````\n"
-                f"{text}\n"
-                "````````\n"
-            )
-            if matches:
-                log += "Classificazioni:\n"
-                for k, v in matches.items():
-                    log += f" - {k}: {v}\n"
+            classificationResult = MyDocument.classify_row(text)
 
-            MyDocument.Log(log + "\n")
+            onlyClass = MyDocument.config["onlyClassifications"]==True and len(classificationResult) > 0
+            allResults = MyDocument.config["onlyClassifications"]==False
+
+            if(onlyClass or allResults):
+                output.append({
+                    "file": os.path.basename(filepath),
+                    "confidance": f"{conf:.2f}",
+                    "coordinations": [float(bbox[0][0]), float(bbox[0][1])],
+                    "text": text,
+                    "classifications": classificationResult
+                })
+
+    output_path = os.path.join(MyDocument.folders["output"], "results.json")
+    with open(output_path, "w", encoding="utf-8") as f:
+        json.dump(output, f, indent=2, ensure_ascii=False)
+
